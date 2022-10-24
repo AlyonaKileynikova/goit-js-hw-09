765b8bf7474f0c657e96fbd48bf5f34fc7dd9519
AlyonaKileynikova <105370125+AlyonaKileynikova@users.noreply.github.com>
2022-10-24 19:06:22 +0300

    Done Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+startBtn.addEventListener('click', () => {
+    intervalId = setInterval( () => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+})
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+})
